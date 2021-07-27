46e2fecf589917bca15105bf12b0e0384cb169e3
MA <victor@clarusway.com>
2021-07-27 23:08:44 +0300

    improved...

--- index.js
+++ index.js
@@ -1,5 +1,297 @@
-console.log('Async Await');
-console.log('-----------');
+// Syncrous Code example 1
+
+// console.clear();
+
+// console.log("starting to execute!");
+
+// for (let index = 0; index < 2000; index++) {
+//     console.log("executing...");
+// }
+
+// console.log("this line has to wait until the previous lines finish their execution.");
+
+// for (let index = 0; index < 10000; index++) {
+//     console.log("another code block.");
+// }
+// console.log("finished.")
+
+
+// Syncrous Code example 2
+// let's create a dummy load image function.
+
+
+// function loadImage(imageName) {
+//     console.log("-------------------------------------------");
+//     console.log("starting to load image file: ", imageName);
+
+//     for (let index = 0; index < 7000; index++) {
+//         console.log("loading the image file...");        
+//     }
+
+//     console.log("load completed.", imageName, " has been loaded.");
+//     console.log("-------------------------------------------");
+// }
+
+
+// console.clear();
+
+// console.log("starting to execute.");
+
+// loadImage("myImage.jpg");
+
+// console.log("finished execution!")
+
+
+
+// Syncrous Code example 3
+// multiple function calls...
+
+
+// function loadImage(imageName, imageSize) {
+//     console.log("-------------------------------------------");
+//     console.log("starting to load image file: ", imageName);
+
+//     for (let index = 0; index < imageSize; index++) {
+//         console.log("loading the image file...");        
+//     }
+
+//     console.log("load completed.", imageName, " has been loaded.");
+//     console.log("-------------------------------------------");
+// }
+
+
+// console.clear();
+
+
+// console.log("starting to execute.");
+
+// loadImage("myImage1.jpg", 2000);  // cut and paste this line to the beginning of the app.
+
+// loadImage("myImage2.jpg", 7000);
+
+// console.log("finished execution!")
+
+
+// Syncrous Code example 4
+// blocking_alert.html sayfasını göster...
+// html dosyasının sonundaki açıklamaları anlat.
+
+
+// yansılardan devam.
+// Async Callbacks yansısı.
+
+
+
+// ASyncrous Code example 1
+
+// console.clear();
+
+// console.log("starting to execute!");
+
+// for (let index = 0; index < 500; index++) {
+//     console.log("executing...");
+// }
+
+// setTimeout(() => console.log("after 5 secs..."), 5000)
+
+// console.log("this line has to wait until the previous lines finish their execution.");
+
+// for (let index = 0; index < 2000; index++) {
+//     console.log("another code block.");
+// }
+
+// console.log("finished.")
+
+
+
+// ASyncrous Code example 2
+
+// console.clear();
+
+// setTimeout(() => console.log("after 10 secs..."), 10000);
+
+// console.log("starting to execute!");
+
+// for (let index = 0; index < 1000; index++) {
+//     console.log("executing...");
+// }
+
+// setTimeout(() => console.log("after 5 secs..."), 5000);
+
+// console.log("this line has to wait until the previous lines finish their execution.");
+
+// for (let index = 0; index < 3000; index++) {
+//     console.log("another code block.");
+// }
+
+// console.log("finished.");
+
+
+
+// ASyncrous Code example 3
+// let's change our function to asyncron mode.
+
+// function loadImage(imageName) {
+//     console.log("-------------------------------------------");
+//     console.log("starting to load image file: ", imageName);
+
+//     for (let index = 0; index < 4000; index++) {
+//         console.log("loading the image file...");        
+//     }
+
+//     console.log("load completed.", imageName, " has been loaded.");
+//     console.log("-------------------------------------------");
+// }
+
+
+// console.log("starting to execute.");
+
+// setTimeout(function () {loadImage("myImage.jpg")}, 5000);
+
+// console.log("finished execution!");
+
+
+
+// ASyncrous Code example 4
+// multiple function calls
+
+// function loadImage(imageName) {
+//     console.log("-------------------------------------------");
+//     console.log("starting to load image file: ", imageName);
+
+//     for (let index = 0; index < 4000; index++) {
+//         console.log("loading the image file...");        
+//     }
+
+//     console.log("load completed.", imageName, " has been loaded.");
+//     console.log("-------------------------------------------");
+// }
+
+
+// setTimeout(function () {loadImage("second image.jpg")}, 5000);
+
+// console.log("starting to execute.");
+
+// setTimeout(function () {loadImage("first image.jpg")}, 7000);
+
+// loadImage("syncron image.jpg");
+
+// console.log("finished execution!");
+
+
+
+
+
+// ASyncrous Code example 5
+
+function readSizeFromDB() {
+
+    let fileSize = 3000;
+
+    console.log("**********************************************************************");
+    console.log("readDB starting...");
+
+    for (let index = 0; index < 1000; index++) {
+        console.log("reading image file name from DB...");        
+    }
+
+    console.log("readDB ending...");
+    console.log("**********************************************************************");
+
+    return fileSize;
+}
+
+function loadImage(imageName, imageSize) {
+    console.log("-------------------------------------------");
+    console.log("starting to load image file: ", imageName, " imageSize: ", imageSize);
+
+    for (let index = 0; index < imageSize; index++) {
+        console.log("loading the image file...");        
+    }
+
+    console.log("load completed.", imageName, " has been loaded.");
+    console.log("-------------------------------------------");
+}
+
+console.log("execution begins.");
+
+// normally, reading from a remote db is an async proccess.
+// here we are trying to simulate.
+let fileSize1 = 0;
+
+console.log("file size first init: ", fileSize1);
+
+fileSize1 = setTimeout(readSizeFromDB, 3000); 
+
+console.log("file size just after let fileSize1: ", fileSize1);
+
+console.log("execution conts.");
+
+setTimeout(function () {loadImage("queue1.jpg", fileSize1)}, 1000);
+
+console.log("file size after load image: ", fileSize1);
+
+console.log("execution ends!")
+
+console.log("file size at the end: ", fileSize1);
+
+
+
+
+// how can we solve this problem?
+
+// function readSizeFromDB() {
+
+//     let fileSize = 3000;
+
+//     console.log("**********************************************************************");
+//     console.log("readDB starting...");
+
+//     for (let index = 0; index < 1000; index++) {
+//         console.log("reading image file name from DB...");        
+//     }
+
+//     console.log("readDB ending...");
+//     console.log("**********************************************************************");
+
+//     return fileSize;
+// }
+
+// function loadImage(imageName, imageSize) {
+//     console.log("-------------------------------------------");
+//     console.log("starting to load image file: ", imageName, " imageSize: ", imageSize);
+
+//     for (let index = 0; index < imageSize; index++) {
+//         console.log("loading the image file...");        
+//     }
+
+//     console.log("load completed.", imageName, " has been loaded.");
+//     console.log("-------------------------------------------");
+// }
+
+
+// setTimeout(() => {
+//     let fileSize1 = readSizeFromDB();
+//     console.log("file size 2: ", fileSize1);
+//     setTimeout(function () {loadImage("queue1.jpg", fileSize1)}, 1000);
+//     console.log("file size 3: ", fileSize1);
+// }, 3000);
+
+
+// setTimeout(() => {
+//     let fileSize = 3000;
+//     setTimeout(() => {
+//         for (let index = 0; index < fileSize; index++) {
+//             console.log("loading the image file...");        
+//         }
+//     }, 1000);
+// }, 10000);
+
+
+
+//////////////////////////////////////////////////////////////////
+// console.log('Async Await');
+// console.log('-----------');
 
 /* function helloWorld(caller) {
     return new Promise((resolve) => {
@@ -27,33 +319,33 @@ for (let i = 0; i < 5; i += 1) {
 }
  */
 
-function grabTheKey(theKey) {
-    console.log(`${theKey} is on my hand.`);
-}
+// function grabTheKey(theKey) {
+//     console.log(`${theKey} is on my hand.`);
+// }
 
-function openTheDoor(theDoor) {
-    console.log(`I'm opening the ${theDoor}`);
-}
+// function openTheDoor(theDoor) {
+//     console.log(`I'm opening the ${theDoor}`);
+// }
 
-function walkInto(thePlace) {
-    console.log(`I'm walking into ${thePlace}`);
-}
+// function walkInto(thePlace) {
+//     console.log(`I'm walking into ${thePlace}`);
+// }
 
-function pickUpThePhone() {
-    console.log(`Talking to my mom!`);
-}
+// function pickUpThePhone() {
+//     console.log(`Talking to my mom!`);
+// }
 
-function textTo(toWhom, message) {
-    console.log(`"${message}" is sent to ${toWhom}`);
-}
+// function textTo(toWhom, message) {
+//     console.log(`"${message}" is sent to ${toWhom}`);
+// }
 
-function ringThePhone() {
-    console.log(`Your phone is ringing`);
-    pickUpThePhone();
-}
+// function ringThePhone() {
+//     console.log(`Your phone is ringing`);
+//     pickUpThePhone();
+// }
 
-setTimeout(ringThePhone(), Math.floor(Math.random() * 2000));
-grabTheKey('Main key');
-textTo('Mike', 'See you tomorrow at 2pm');
-openTheDoor('Main door');
-walkInto('My Home');
+// setTimeout(ringThePhone(), Math.floor(Math.random() * 2000));
+// grabTheKey('Main key');
+// textTo('Mike', 'See you tomorrow at 2pm');
+// openTheDoor('Main door');
+// walkInto('My Home');
